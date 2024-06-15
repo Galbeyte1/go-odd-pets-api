2b694113399ecc225389c53007bb35ee03886b44
Galbeyte <s.galbeyte@gmail.com>
2024-06-15 15:56:52 -0500

    Configurations and logging

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,35 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log/slog"
+	"os"
+)
+
+type config struct {
+	addr string
+}
+
+var Logger = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+	Level:     slog.LevelDebug,
+	AddSource: true,
+}))
+
+var cfg config
+
+func init() {
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+}
 
 // Run - responsible for the instantiation and startup
 func Run() error {
-	log.Println("Starting up API")
+	Logger.Info("Starting up API on", "addr", cfg.addr)
 	return nil
 }
 
 func main() {
-	log.Println("Welcome!")
+	Logger.Info("Welcome!")
 	if err := Run(); err != nil {
-		log.Println(err)
+		Logger.Error("Error", err)
 	}
 }
